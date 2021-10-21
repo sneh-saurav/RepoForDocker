1aa925e49f7f8d1727772be89e92c59468dd7138
Sneh Saurav <79414816+sneh-saurav@users.noreply.github.com>
2021-10-21 14:21:09 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func print(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "This App is Running")
-	fmt.Fprint(w,"Hook is triggered")  // Just for checking for web-hooks
+	fmt.Fprint(w,"Hook is triggered")  // Just for checking for web-hooks again
 }
 
 func main() {
