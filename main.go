09834a717406becb9226cafae99176f4d12dfb9c
Sneh Saurav <79414816+sneh-saurav@users.noreply.github.com>
2021-12-04 15:56:49 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func print(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "This App is Running")
-	fmt.Fprintln(w,"Hook is triggered")   // New comment again
+	fmt.Fprintln(w," I hope Hook is been triggered")   // New comment again
 }
 
 func main() {
